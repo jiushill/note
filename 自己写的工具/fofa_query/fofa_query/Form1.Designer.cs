ad5f2382c203c5d0c5aa0d7c2c628fbe79c3ef72
九世 <422926799@qq.com>
2021-02-05 01:27:16 +0800

    Add files via upload

---
+++ 自己写的工具/fofa_query/fofa_query/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace fofa_query
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.threadset = new System.Windows.Forms.ComboBox();
+            this.response = new System.Windows.Forms.ListView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.page = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.保存到文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.清空ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.复制所选ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label10 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(46, 52);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(613, 25);
+            this.key.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "多线程";
+            // 
+            // threadset
+            // 
+            this.threadset.FormattingEnabled = true;
+            this.threadset.Location = new System.Drawing.Point(399, 129);
+            this.threadset.Name = "threadset";
+            this.threadset.Size = new System.Drawing.Size(121, 23);
+            this.threadset.TabIndex = 3;
+            this.threadset.Text = "5";
+            // 
+            // response
+            // 
+            this.response.ContextMenuStrip = this.contextMenuStrip1;
+            this.response.Location = new System.Drawing.Point(15, 177);
+            this.response.Name = "response";
+            this.response.Size = new System.Drawing.Size(638, 294);
+            this.response.TabIndex = 4;
+            this.response.UseCompatibleStateImageBehavior = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 490);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "返回内容";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "查询语法";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(82, 86);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(571, 25);
+            this.search.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "页数";
+            // 
+            // page
+            // 
+            this.page.Location = new System.Drawing.Point(62, 125);
+            this.page.Name = "page";
+            this.page.Size = new System.Drawing.Size(121, 25);
+            this.page.TabIndex = 9;
+            this.page.Text = "1";
+            this.page.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.page_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(524, 490);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "已收集的IP数量:0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(545, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 15);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "芽衣,琪亚娜";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(2, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 15);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "email";
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(46, 18);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(613, 25);
+            this.email.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(293, 490);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(90, 15);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "查询语法为:";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.保存到文件ToolStripMenuItem,
+            this.清空ToolStripMenuItem,
+            this.复制所选ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(154, 76);
+            // 
+            // 保存到文件ToolStripMenuItem
+            // 
+            this.保存到文件ToolStripMenuItem.Name = "保存到文件ToolStripMenuItem";
+            this.保存到文件ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.保存到文件ToolStripMenuItem.Text = "保存到文件";
+            this.保存到文件ToolStripMenuItem.Click += new System.EventHandler(this.保存到文件ToolStripMenuItem_Click);
+            // 
+            // 清空ToolStripMenuItem
+            // 
+            this.清空ToolStripMenuItem.Name = "清空ToolStripMenuItem";
+            this.清空ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.清空ToolStripMenuItem.Text = "清空";
+            this.清空ToolStripMenuItem.Click += new System.EventHandler(this.清空ToolStripMenuItem_Click);
+            // 
+            // 复制所选ToolStripMenuItem
+            // 
+            this.复制所选ToolStripMenuItem.Name = "复制所选ToolStripMenuItem";
+            this.复制所选ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.复制所选ToolStripMenuItem.Text = "复制所选";
+            this.复制所选ToolStripMenuItem.Click += new System.EventHandler(this.复制所选ToolStripMenuItem_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(138, 490);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(45, 15);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "页数:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 514);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.page);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.response);
+            this.Controls.Add(this.threadset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.key);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "FOFA API查询";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox threadset;
+        private System.Windows.Forms.ListView response;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox page;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 保存到文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 清空ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 复制所选ToolStripMenuItem;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ 自己写的工具/fofa_query/fofa_query/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using Amib.Threading;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+
+namespace fofa_query
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            threadsetfunc();
+            responseload();
+        }
+
+        private static  SmartThreadPool stp = new SmartThreadPool(); //定义线程池
+        private static string querystr=""; //返回的查询语法记录
+        private static int numberip = 0; //收集的IP数量
+        private static List<string> urls = new List<string>();
+        private static int tmppage = 0;
+
+        private static void stopthreadpool()
+        {
+            stp.Cancel();
+        }
+
+        //多线程设置
+        private void threadsetfunc() {
+            int[] threadlist = { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 200 };
+            foreach (var td in threadlist) {
+                threadset.Items.Add(td);
+            }
+        }
+
+        //返回内存UI设置
+        private void responseload() {
+            response.GridLines = true;
+            response.FullRowSelect = true;
+            response.View = View.Details;
+            response.Scrollable = true;
+            response.MultiSelect = true;
+            response.Columns.Add("IP", 160, HorizontalAlignment.Center);
+            response.Columns.Add("URL", 160, HorizontalAlignment.Center);
+            response.Columns.Add("PORT", 160, HorizontalAlignment.Center);
+        }
+
+        //异步委托
+        public class Query {
+            public delegate void Updateui(string ip,string url,string port);
+            public Updateui update;
+            public delegate void Out();
+            public Out outs;
+
+            public void stop() {
+                while (true) {
+                    if (stp.IsIdle == true) {
+                        break;
+                    }
+                }
+                outs();
+            }
+            public void query(string url)
+            {
+                    try
+                    {
+                        tmppage += 1;
+                        HttpWebRequest requests = (HttpWebRequest)WebRequest.Create(url);
+                        requests.Method = "GET";
+                        requests.Headers.Add("UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36");
+                        HttpWebResponse response = (HttpWebResponse)requests.GetResponse();
+                        StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("utf-8"));
+                        string html = sr.ReadToEnd();
+                        var Name = JObject.Parse(html);
+                        string error = Name["error"].ToString();
+                        string resultquery = Name["query"].ToString().Replace("\\", "");
+                        querystr = resultquery;
+                        if (error != "false")
+                        {
+                            if (Name["results"].Count() != 0)
+                            {
+                                foreach (var result in Name["results"])
+                                {
+                                    string url_ = result[0].ToString();
+                                    string ip = result[1].ToString();
+                                    string port = result[2].ToString();
+                                    numberip += 1;
+                                    update(ip, url_, port);
+                                }
+                            }
+                            else
+                            {
+                                update("", "", "");
+                            }
+                        }
+                        else
+                        {
+                            update("", "", "");
+                        }
+                    }
+                    catch (WebException Ex)
+                    {
+                        MessageBox.Show("请求出现异常:" + Ex.ToString());
+                        stopthreadpool();
+                    }
+            }
+        }
+
+        //开始或暂停
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //恢复初始化
+            numberip = 0;
+            tmppage = 0;
+            urls.Clear();
+            Query q = new Query();
+            q.update += updateUI;
+            q.outs += outs;
+            label10.Text = "当前页数:0/0";
+            if (button1.Text == "停止")
+            {
+                stp.Cancel(true);
+                button1.Text = "开始";
+                button1.Refresh();
+            }
+            else
+            {
+                if (email.TextLength != 0 && key.TextLength != 0 && search.TextLength != 0 && page.TextLength != 0)
+                {
+                    label10.Text = "当前页数:0/" + page.Text.ToString();
+                    button1.Text = "停止";
+                    string search_base64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(search.Text.ToString())).Replace("+", "%2b");
+                    stp.MaxQueueLength = Convert.ToInt32(threadset.Text.ToString());
+                    for (int p = 0; p < Convert.ToInt32(page.Text.ToString()); p++)
+                    {
+                        string fofaurl = "https://fofa.so/api/v1/search/all?email=" + email.Text.ToString() + "&key=" + key.Text.ToString() + "&qbase64=" + search_base64 + "&page=" + p;
+                        stp.QueueWorkItem<string>(q.query, fofaurl);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("检查邮箱/key/搜索语法/页面是否填写错误");
+                }
+                SmartThreadPool tmp = new SmartThreadPool();
+                tmp.QueueWorkItem(q.stop);
+            }
+
+        }
+
+        delegate void asyncupdate(string ip, string url, string port);
+        delegate void Outs();
+        //更新UI
+        private void updateUI(string ip, string url, string port) {
+            if (InvokeRequired)
+            {
+                this.Invoke(new asyncupdate(delegate (string ip_, string url_, string port_)
+                {
+                    label10.Text = "页数:" + tmppage + "/" + page.Text.ToString();
+                    label10.Refresh();
+                    label9.Text = "查询语法为:" + querystr.ToString();
+                    label9.Refresh();
+                    if (ip.Length != 0 && port.Length != 0)
+                    {
+                        ListViewItem item = new ListViewItem();
+                        item.SubItems.Clear();
+                        item.SubItems[0].Text = ip;
+                        item.SubItems.Add(url);
+                        item.SubItems.Add(port);
+                        response.Items.Add(item);
+                        label6.Text = "已收集的IP数量:" + numberip.ToString();
+                        label6.Refresh();
+                        response.Items[response.Items.Count - 1].EnsureVisible();
+                    }
+                }), ip, url, port);
+
+            }
+            else {
+                label10.Text = "页数:" + tmppage + "/" + page.Text.ToString();
+                label10.Refresh();
+                label9.Text = "查询语法为:" + querystr.ToString();
+                label9.Refresh();
+                if (ip.Length != 0 && port.Length != 0)
+                {
+                    ListViewItem item = new ListViewItem();
+                    item.SubItems.Clear();
+                    item.SubItems[0].Text = ip;
+                    item.SubItems.Add(url);
+                    item.SubItems.Add(port);
+                    response.Items.Add(item);
+                    label6.Text = "已收集的IP数量:" + numberip.ToString();
+                    label6.Refresh();
+                    response.Items[response.Items.Count - 1].EnsureVisible();
+                }
+            }
+        }
+
+        //任务完成
+        private void outs() {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Outs(delegate ()
+                {
+                    button1.Text = "开始";
+                    button1.Refresh();
+                    MessageBox.Show("任务完成");
+                }));
+            }
+            else {
+                this.Invoke(new Outs(delegate ()
+                {
+                    button1.Text = "开始";
+                    button1.Refresh();
+                    MessageBox.Show("任务完成");
+                }));
+            }
+        }
+
+        //限制page只能输入数字
+        private void page_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8){
+                    e.Handled = true;
+                }
+        }
+
+        //清空获取的结果
+        private static void deleteitems(ListView datasx) {
+            for (int c = 0; c < datasx.Items.Count; c++) {
+                datasx.Items.RemoveAt(c);
+            }
+
+            for (int c = 0; c < datasx.Items.Count; c++)
+            {
+                deleteitems(datasx);
+            }
+        }
+
+        private void 保存到文件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var savedialog = new System.Windows.Forms.SaveFileDialog();
+            savedialog.Filter = "txt (*.txt)|*.txt";
+            savedialog.FileName = "save.txt";
+            savedialog.ShowDialog();
+            FileStream f = new FileStream(savedialog.FileName,FileMode.Create);
+            StreamWriter fw = new StreamWriter(f);
+            for (int calc = 0; calc < response.Items.Count; calc++) {
+                var d = response.Items[calc].SubItems;
+                string ip = d[0].Text;
+                string url = d[1].Text;
+                string port = d[2].Text;
+                string output = "IP:" + ip + " url:" + url + " port:" + port;
+                fw.WriteLine(output);
+            }
+            fw.Close();
+            f.Close();
+            MessageBox.Show("文件保存在:"+savedialog.FileName);
+        }
+
+        private void 复制所选ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var selectdata = response.SelectedItems;
+            var d = response.Items[0].SubItems;
+            string ip = d[0].Text;
+            string url = d[1].Text;
+            string port = d[2].Text;
+            string output = "IP:" + ip + " url:" + url + " port:" + port;
+            MessageBox.Show(output);
+            Clipboard.SetDataObject(output);
+        }
+
+        private void 清空ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            deleteitems(response);
+        }
+    }
+}
